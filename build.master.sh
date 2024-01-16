086cb989e11fb03548526c2fedfe52f4ec949131
Lukasz Bromirski <lukasz@bromirski.net>
2024-01-16 01:35:54 +0100

    fix time callout

--- build.master.sh
+++ build.master.sh
@@ -15,10 +15,10 @@ ${MAKE} clean >/dev/null
 git pull > /usr/src/build.$DATE.git.txt
 
 # Build process
-time -h ${MAKE} buildworld > /usr/src/build.$DATE.world.txt
+/usr/bin/time -h ${MAKE} buildworld > /usr/src/build.$DATE.world.txt
 if [ $? -eq 0 ]; then
    echo "Process finished, moving to kernel" >> /usr/src/build.$DATE.world.txt
-   time -h ${MAKE} ${KERNCONF_STR} kernel > /usr/src/build.$DATE.kernel.txt
+   /usr/bin/time -h ${MAKE} ${KERNCONF_STR} kernel > /usr/src/build.$DATE.kernel.txt
    if [ $? -eq 0 ]; then
         echo "Process finished, build complete" >> /usr/src/build.$DATE.kernel.txt
    else
