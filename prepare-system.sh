fc1f8248a61cba478e0b9732060d9a23d1367d4c
Lukasz Bromirski <lukasz@bromirski.net>
2026-05-30 10:26:29 +0200

    proper order for sshd hardening

--- prepare-system.sh
+++ prepare-system.sh
@@ -3,12 +3,12 @@
 # OpenSSH
 
 echo "Updating OpenSSH to hardened and key-only auth"
+awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.safe
+mv /etc/ssh/moduli.safe /etc/ssh/moduli
+
 rm /etc/ssh/ssh_host_*
 ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ""
 ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""
-
-awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.safe
-mv /etc/ssh/moduli.safe /etc/ssh/moduli
 
 mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old
 cp sshd_config /etc/ssh/
